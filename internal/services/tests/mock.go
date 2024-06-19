97b96e3c3fa8d8e039ad1574a95347c48c75b2c4
Kvothe838 <facundoyuffrida@gmail.com>
2024-06-19 12:17:11 +0200

    Implemented tests for services: GetQuiz

--- internal/services/tests/mock.go
+++ internal/services/tests/mock.go
@@ -1,12 +1,55 @@
 package services_tests
 
 import (
+	"context"
+	"github.com/Kvothe838/fast-track-test-quiz/internal/models"
 	"github.com/golang/mock/gomock"
+	"reflect"
 )
 
 type MockRepository struct {
 	ctrl     *gomock.Controller
 	recorder *MockRepositoryRecorder
+}
+
+func (m *MockRepository) CreateChoiceSelection(ctx context.Context, selection models.ChoiceSelection) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *MockRepository) DoesChoiceSelectionExist(ctx context.Context, selection models.ChoiceSelection) (bool, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *MockRepository) GetChoiceSelections(ctx context.Context) ([]models.ChoiceSelection, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *MockRepository) GetAllQuizSubmissions(ctx context.Context) ([]models.QuizSubmission, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *MockRepository) GetQuizSubmission(ctx context.Context, submissionID int) (models.QuizSubmission, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *MockRepository) SaveQuizSubmission(ctx context.Context) (models.QuizSubmission, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *MockRepository) UpdateChoiceSelection(ctx context.Context, selection models.ChoiceSelection) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *MockRepository) UpdateSubmission(ctx context.Context, submission models.QuizSubmission) error {
+	//TODO implement me
+	panic("implement me")
 }
 
 type MockRepositoryRecorder struct {
@@ -23,4 +66,17 @@ func NewMockRepository(ctrl *gomock.Controller) *MockRepository {
 
 func (m *MockRepository) EXPECT() *MockRepositoryRecorder {
 	return m.recorder
+}
+
+func (m *MockRepository) GetQuiz(ctx context.Context) (models.Quiz, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetQuiz", ctx)
+	ret0, _ := ret[0].(models.Quiz)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (mr *MockRepositoryRecorder) GetQuiz(ctx interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetQuiz", reflect.TypeOf((*MockRepository)(nil).GetQuiz), ctx)
 }
---
+++ internal/services/tests/quizes_test.go
@@ -0,0 +1,74 @@
+package services_tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Kvothe838/fast-track-test-quiz/internal/models"
+	"github.com/Kvothe838/fast-track-test-quiz/internal/services"
+	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetQuiz(t *testing.T) {
+	tests := []struct {
+		name           string
+		prepareRepo    func(*MockRepository)
+		assertOnResult func(campaign models.Quiz, err error)
+	}{
+		{
+			name: "success on GetQuiz",
+			prepareRepo: func(mock *MockRepository) {
+				mock.EXPECT().GetQuiz(
+					gomock.Any(),
+				).Return(
+					models.Quiz{
+						Title:       "test title",
+						Description: "test description",
+						Questions:   nil,
+					},
+					nil,
+				)
+			},
+			assertOnResult: func(quiz models.Quiz, err error) {
+				assert.NoError(t, err)
+				assert.Equal(t, "test title", quiz.Title, "quiz must have 'test title' as title")
+				assert.Equal(t, "test description", quiz.Description, "quiz must have 'test description' as description")
+				assert.Nil(t, quiz.Questions, "questions must be nil")
+			},
+		},
+		{
+			name: "error on GetQuiz",
+			prepareRepo: func(mock *MockRepository) {
+				mock.EXPECT().GetQuiz(
+					gomock.Any(),
+				).Return(
+					models.Quiz{},
+					errors.New("test error"),
+				)
+			},
+			assertOnResult: func(quiz models.Quiz, err error) {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), "test error")
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			repo := NewMockRepository(gomock.NewController(t))
+
+			if test.prepareRepo != nil {
+				test.prepareRepo(repo)
+			}
+
+			in := services.NewInteractor(repo)
+			quiz, err := in.GetQuiz(context.Background())
+
+			if test.assertOnResult != nil {
+				test.assertOnResult(quiz, err)
+			}
+		})
+	}
+}
